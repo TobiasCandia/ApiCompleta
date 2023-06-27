8e75921583a47ba96a50e6622c7aaf35a5977e63
TobiasCandia <tobiascandia93@gmail.com>
2023-06-27 10:09:10 -0300

    Creada la cadena de conexion.

---
+++ ApiCompleta/Datos/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiCompleta.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCompleta.Datos
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Villa> Villas {get; set;}
+	}
+}
